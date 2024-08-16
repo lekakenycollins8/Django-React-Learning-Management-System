0ebeea69b76f65978b600b8dc82cd3d208474ef6
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-08-16 10:46:26 +0300

    created gegister user serializer and methods for password validation

--- backend/backend/api/serializer.py
+++ backend/backend/api/serializer.py
@@ -3,6 +3,7 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from userauths.models import User, Profile
+from django.contrib.auth.password_validation import validate_password
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     """Custom token obtain pair serializer: adds full name, email, and username to the token"""
@@ -14,6 +15,35 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['username'] = user.username
 
         return token
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """Register serializer"""
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('full_name', 'email', 'password', 'password2')
+
+    def validate(self, attr):
+        """validate password for match"""
+        if attr['password'] != attr['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match"})
+        return attr
+
+    def create(self, validated_data):
+        """Create user"""
+        user = User.objects.create_user(
+            full_name=validated_data['full_name'],
+            email=validated_data['email'],
+        )
+
+        email_username, _ = user.email.split('@')
+        user.username = email_username
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
 
 class UserSerializer(serializers.ModelSerializer):
     """User serializer"""
