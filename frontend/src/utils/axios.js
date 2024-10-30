f0fe8d7a54e86a9f90077353b79a09506f50255d
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-10-30 10:54:48 +0300

    Refactor axios configuration to use API base URL constant

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_BASE_URL } from './constants';
 
 const apiInstance = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: API_BASE_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
