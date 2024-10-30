bbf1e1ab108d5adb407c7d027b20694358731c86
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-10-30 11:33:29 +0300

    Refactor PrivateRoute component to add prop type validation for children

--- frontend/src/layouts/PrivateRoute.js
+++ frontend/src/layouts/PrivateRoute.js
@@ -1,10 +1,14 @@
 import { Navigate } from 'react-router-dom';
 import { useAuthStore } from '../store/auth';
+import PropTypes from 'prop-types';
 
 const PrivateRoute = ({ children }) => {
     const loggedIn = useAuthStore((state) => state.isLoggedIn)();
 
     return loggedIn ? <>{children}</> : <Navigate to='/login' />;
+};
+PrivateRoute.propTypes = {
+    children: PropTypes.node.isRequired,
 };
 
 export default PrivateRoute;
